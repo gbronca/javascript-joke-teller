6c3af8bcf457706f8c173491422240e48302ad4b
gbronca <42703789+gbronca@users.noreply.github.com>
2020-08-16 16:05:25 +0100

    parse jokes from JokeAPI to VoiceRSS API

---
+++ README.md
@@ -0,0 +1,5 @@
+# Joke Teller
+
+Simple and fun Text-to-Speech joke teller.
+
+It uses the [Joke API](https://sv443.net/jokeapi/v2/) to fetch the jokes and [VoiceRSS API](http://www.voicerss.org/) to convert to speech.
--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 const button = document.getElementById('button');
 const audioElement = document.getElementById('audio');
 
-// function test() {
-// 	VoiceRSS.speech({
-// 		key: '99e221e2aa6c4d5dafd60023f01266ec',
-// 		src: 'Hello, world!',
-// 		hl: 'en-us',
-// 		v: 'Linda',
-// 		r: 0, 
-// 		c: 'mp3',
-// 		f: '44khz_16bit_stereo',
-// 		ssml: false
-// 	});
-// }
+// Disabled/Enabled button
+function toggleButton() {
+	button.disabled = !button.disabled;
+}
 
-// test();
+// Passing Joke to VoiceRSS API
+function tellMe(joke) {
+	VoiceRSS.speech({
+		key: '99e221e2aa6c4d5dafd60023f01266ec',
+		src: joke,
+		hl: 'en-us',
+		v: 'Linda',
+		r: 0, 
+		c: 'mp3',
+		f: '44khz_16bit_stereo',
+		ssml: false
+	});
+}
 
 // Get Jokes from Joke API
 async function getJokes() {
@@ -28,10 +32,15 @@ async function getJokes() {
 		} else {
 			joke = data.joke;
 		}
-		console.log(joke);
+		// Text-to-Speech
+		tellMe(joke);
+		// Disable Button
+		toggleButton();
 	} catch (error) {
 		console.log('error', error);
 	}
 }
 
-getJokes();
+// Event Listeners
+button.addEventListener('click', getJokes);
+audioElement.addEventListener('ended', toggleButton);
